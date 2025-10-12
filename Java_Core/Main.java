727ee756af81d17ec725b71de0c4b4f2146dfc20
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-12 22:32:46 +0530

    update

--- Java_Core/Main.java
+++ Java_Core/Main.java
@@ -4,22 +4,36 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("SF 2027");
 
-        int num = 9;
-        byte by = 127;
-        short sh = 558;
-        long l = 5854l;
-        float f = 123.456f;
-        double d = 5.8d;
-        char ch = 'A';
-        boolean b = true;
-
         //literals: the values of a variable
-        int num1 = 0b1010;   //binary
-        int num2 = 0x7E;    //hexa
-        int num3 = 1_00_00_000;
-        float num4 = 56f;
-        double num5 = 12e10;
-        System.out.println(num1);
+//        int num1 = 0b1010;   //binary
+//        int num2 = 0x7E;    //hexa
+//        int num3 = 1_00_00_000;
+//        float num4 = 56f;
+//        double num5 = 12e10;
+//        System.out.println(num1);
+
+        // Type Conversion
+//        byte b = 127;
+//        int a = b;
+//        a = 259;
+//        b = (byte) a;   // 259 out of range. will take a%256. EXPLICIT CASTING
+//        System.out.println(b);
+//        System.out.println(a);
+//
+//        float f = 56.96f;
+//        int t = (int)f; //EXPLICIT CASTING
+//        System.out.println(t);
+//        float f1 = f + t;
+//        System.out.println(f1); //TYPE PROMOTION
+
+        // Arithmetic Operation
+        int a = 6;
+        a--;
+        //System.out.println(a);
+        ++a;
+        System.out.println(a);
+
+
 
     }
 }
