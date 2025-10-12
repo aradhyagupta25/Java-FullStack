9853d3b17d1c90a7a02c28533d1bf342ae8988e7
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-12 14:41:41 +0530

    Auto update

--- Java_Core/Main.java
+++ Java_Core/Main.java
@@ -3,5 +3,15 @@ package Java_Core;
 public class Main {
     public static void main(String[] args) {
         System.out.println("SF 2027");
+
+        int num = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+        float f = 123.456f;
+        double d = 5.8d;
+        char ch = 'A';
+        boolean b = true;
+
     }
 }
---
+++ update.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "D:\CODING\Java FullStack"
+git add .
+git commit -m "Auto update"
+git push origin main
+pause
