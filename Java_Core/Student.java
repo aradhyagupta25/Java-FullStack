5e953944418c8e82a83ffd9bc853ddfcccab9350
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-14 09:21:10 +0530

    method overloading

--- Java_Core/Student.java
+++ Java_Core/Student.java
@@ -8,14 +8,20 @@ public class Student {
         int grade = mark / 100;
         return grade ;
     }
+    public float marks(float mark){
+        float grade = mark / 100;
+        return grade ;
+    }
 
     public static void main(String[] args) {
         Student s1 = new Student();
         // type(Student) ref_variable(s1) = memory_alloc(new) Constructor(Student()
 
-        int final_grade = s1.marks(837);
-        System.out.println(final_grade);
-        System.out.println("hello");
+        int final_grade1 = s1.marks(837);
+        float final_grade2 = s1.marks(923.45f);
+        System.out.println(final_grade1);
+        System.out.println(final_grade2);
+
     }
 }
 
--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "D:\CODING\Java FullStack"
 git add .
-git commit -m "Auto update"
+git commit -m "Daily Commit"
 git push origin main
 pause
