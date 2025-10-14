563857bff2cd19a0074ca0c3eaf2b71fd11bbd21
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-14 16:59:39 +0530

    2D Arrays

--- Java_Core/Arrays/Demo.java
+++ Java_Core/Arrays/Demo.java
@@ -12,7 +12,20 @@ public class Demo {
         float num1[] = new float[4];    // dynamic allocation
         System.out.println(num1[2]);    // all zero values
 
+        int array2d[][] = new int[4][4];    // 2D Array
 
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                array2d[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(array2d[i][j] + " ");
+            }
+            System.out.println("");
+        }
 
 
     }
