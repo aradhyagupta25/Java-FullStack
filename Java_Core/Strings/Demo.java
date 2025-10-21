6afdc5caa538a2b3d06aa6dd6a7acaf29ec5ef0c
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-21 17:37:08 +0530

    Immutable Strings

--- Java_Core/Strings/Demo.java
+++ Java_Core/Strings/Demo.java
@@ -3,14 +3,17 @@ package Java_Core.Strings;
 public class Demo {
     public static void main(String[] args) {
 
-        String name1 = "Aradhya";    // String is a class.
-        System.out.println(name1 + " " + name1);
+        String n1 = "Aradhya";    // String is a class.
+        System.out.println("Name:" + " " + n1);
 
-        String name2 = new String("okay");  // correct method to define a String by creating it's object and passing the value to constructor ().
-        System.out.println(name2 + " " + name1);
-        System.out.println(name1.charAt(6));
-        System.out.println(name1.concat(" Gupta")); // No change in the original
+        String n2 = new String("Gupta");  // correct method to define a String by creating it's object and passing the value to constructor ().
+        System.out.println(n1 + " " + n2);
+        System.out.println(n1.charAt(6));
+        System.out.println(n1.concat(" Gupta")); // No change in the original
 
+        String s1 = "George";
+        String s2 = "George";
+        System.out.println(s1 == s2); // same ref add for both s1 and s2.
 
     }
 }
