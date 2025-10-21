e8c7ca044d8a85141c98672267cd10889e5dd1f7
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-21 16:57:59 +0530

    Daily Commit

--- Java_Core/Strings/Demo.java
+++ Java_Core/Strings/Demo.java
@@ -3,13 +3,13 @@ package Java_Core.Strings;
 public class Demo {
     public static void main(String[] args) {
 
-        String name = "Aradhya";    // String is a class.
-        System.out.println(name + " " + name);
+        String name1 = "Aradhya";    // String is a class.
+        System.out.println(name1 + " " + name1);
 
         String name2 = new String("okay");  // correct method to define a String by creating it's object and passing the value to constructor ().
-        System.out.println(name2 + " " + name);
-        System.out.println(name.charAt(6));
-        System.out.println(name.concat(" Gupta")); // No change in the original
+        System.out.println(name2 + " " + name1);
+        System.out.println(name1.charAt(6));
+        System.out.println(name1.concat(" Gupta")); // No change in the original
 
 
     }
