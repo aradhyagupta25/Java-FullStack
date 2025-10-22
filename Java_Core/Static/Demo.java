fcb1573ae1ea067a63c9d181a294456e44074c16
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-22 13:45:33 +0530

    static methods and variables

--- Java_Core/Static/Demo.java
+++ Java_Core/Static/Demo.java
@@ -1,9 +1,11 @@
 package Java_Core.Static;
 
+import jdk.dynalink.beans.StaticClass;
+
 class Mobile{
     String brand;
     int price;
-    String type;
+    static String type = "Smart";   // static variable. it belongs to the class "Mobile" not obj.
 
     public void show(){
         System.out.println(brand + " : " + price + " : " + type);
@@ -15,12 +17,12 @@ public class Demo {
         Mobile m1 = new Mobile();
         m1.brand = "Apple";
         m1.price = 1500;
-        m1.type = "Smart";
+        //m1.type = "Smart";
 
         Mobile m2 = new Mobile();
         m2.brand = "Samsung";
         m2.price = 1000;
-        m2.type = "Smart";
+        //m2.type = "Smart";
 
         m1.show();
         m2.show();
