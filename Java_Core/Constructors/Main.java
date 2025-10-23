bfb58442fd8378271edf30548455e2e571188c19
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-23 18:26:25 +0530

    Constructor

--- Java_Core/Constructors/Main.java
+++ Java_Core/Constructors/Main.java
@@ -5,16 +5,23 @@ class Car{
     String model;
     int price;
 
-    Car(){
+    Car(){      // never returns anything
         this.brand = "Car";
         this.model = "Car";
         this.price = 200;
+        System.out.println("Inside Constructor");
+        System.out.println(this.brand + " : " + this.model + " : " + this.price);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+        //System.out.println(car.brand + " " +  car.model + " " + car.price);
+        car.brand = "BMW";
+        car.model = "3 Series";
+        car.price = 50_000;
+        System.out.println(car.brand + " " +  car.model + " " + car.price);
 
     }
 }
