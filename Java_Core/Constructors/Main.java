0e2fd5d3f3ab1c9ab36436e7c1fbfd50e18c2d64
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-23 18:48:03 +0530

    Parameterized  Constructor

--- Java_Core/Constructors/Main.java
+++ Java_Core/Constructors/Main.java
@@ -5,10 +5,10 @@ class Car{
     String model;
     int price;
 
-    Car(){      // never returns anything
-        this.brand = "Car";
-        this.model = "Car";
-        this.price = 200;
+    Car(String brand, String model, int price){      // never returns anything
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
         System.out.println("Inside Constructor");
         System.out.println(this.brand + " : " + this.model + " : " + this.price);
     }
@@ -16,8 +16,8 @@ class Car{
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        //System.out.println(car.brand + " " +  car.model + " " + car.price);
+        Car car = new Car("FERRARI", "458 ITALIA", 1000000);
+        System.out.println(car.brand + " " +  car.model + " " + car.price);
         car.brand = "BMW";
         car.model = "3 Series";
         car.price = 50_000;
