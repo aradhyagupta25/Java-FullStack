376c4064b9449647565cb298cccc596309c67874
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-25 10:51:51 +0530

    Ingeritance

--- Java_Core/Inheritance/Main.java
+++ Java_Core/Inheritance/Main.java
@@ -11,7 +11,22 @@ class Calc{
 }
 
 public class Main extends Calc{
+
+    public int multi(int a, int b){
+        return a * b;
+    }
+
+     public int div(int a, int b){
+        return a / b;
+     }
+
     public static void main(String[] args) {
+
+        Main c1 = new Main();
+        System.out.println(c1.add(1,2));
+        System.out.println(c1.sub(2,3));
+        System.out.println(c1.multi(3,4));
+        System.out.println(c1.div(5,6));
 
     }
 }
