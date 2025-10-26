eeaa6d0d915d337949da5f9aa5ed3161581f32f6
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-26 18:33:24 +0530

    Upcasting and Downcasting

---
+++ Java_Core/Up_DownCasting/Computer.java
@@ -0,0 +1,24 @@
+package Java_Core.Up_DownCasting;
+
+class Laptop{
+    public void show1(){
+        System.out.println(" Inside Laptop");
+    }
+}
+class PC extends Laptop{
+    public void show2(){
+        System.out.println(" Inside PC");
+    }
+}
+
+public class Computer {
+    public static void main(String[] args) {
+
+        Laptop lap =  new PC(); // up casting to parent class type.
+        lap.show1();
+        //lap.show2(); Can't use method of PC when the type of ref var is Laptop.
+
+        PC pc = (PC) lap;
+        pc.show2(); // Down Casting to child class type.
+    }
+}
