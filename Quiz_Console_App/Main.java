8f7405534eed073d635d3c789cd0164977c068cb
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-28 22:53:08 +0530

    Quiz Console App

--- Quiz_Console_App/Main.java
+++ Quiz_Console_App/Main.java
@@ -2,6 +2,7 @@ package Quiz_Console_App;
 
 public class Main {
     public static void main(String[] args) {
-
+        QuestionService service = new QuestionService();
+        service.displayQuestions();
     }
 }
