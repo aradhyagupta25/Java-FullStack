709fb90dbe4aa7b31215abea3d06e67551e6c60b
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-28 23:42:44 +0530

    Quiz Console App

--- Quiz_Console_App/QuestionService.java
+++ Quiz_Console_App/QuestionService.java
@@ -3,6 +3,10 @@ package Quiz_Console_App;
 public class QuestionService {
     Questions[] questions = new Questions[5];
 
+    public QuestionService(){
+        questions[0] = new Questions(1, "What", "java", "c++", "javascript", "python", "java");
+    }
+
     public void displayQuestions() {
         System.out.println("In Display Questions");
 
--- Quiz_Console_App/Questions.java
+++ Quiz_Console_App/Questions.java
@@ -9,7 +9,15 @@ public class Questions {
     private String opt4;
     private String answer;
 
-
+    public Questions(int  id, String question, String opt1, String opt2, String opt3, String opt4, String answer) {
+        this.id = id;
+        this.question = question;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.opt3 = opt3;
+        this.opt4 = opt4;
+        this.answer = answer;
+    }
 
     public int getId() {
         return id;
