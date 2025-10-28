bc93f8a3d1962a842d970d2cd254e05ca6a63d5e
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-29 00:23:29 +0530

    Quiz Console App

--- Quiz_Console_App/QuestionService.java
+++ Quiz_Console_App/QuestionService.java
@@ -4,7 +4,11 @@ public class QuestionService {
     Questions[] questions = new Questions[5];
 
     public QuestionService(){
-        questions[0] = new Questions(1, "What", "java", "c++", "javascript", "python", "java");
+        questions[0] = new Questions(1, "Which lang", "java", "c++", "javascript", "python", "java");
+        questions[1] = new Questions(2, "Which car", "java", "c++", "javascript", "python", "java");
+        questions[2] = new Questions(3, "Which city", "java", "c++", "javascript", "python", "java");
+        questions[3] = new Questions(4, "Which ", "java", "c++", "javascript", "python", "java");
+        questions[4] = new Questions(5, "What", "java", "c++", "javascript", "python", "java");
     }
 
     public void displayQuestions() {
