4e28cd32705f74fb33064d9f90b02abc9d27f2df
Aradhya Gupta <personalsomu25@gmail.com>
2025-10-28 23:20:28 +0530

    Quiz Console App

--- Quiz_Console_App/QuestionService.java
+++ Quiz_Console_App/QuestionService.java
@@ -2,4 +2,9 @@ package Quiz_Console_App;
 
 public class QuestionService {
     Questions[] questions = new Questions[5];
+
+    public void displayQuestions() {
+        System.out.println("In Display Questions");
+
+    }
 }
