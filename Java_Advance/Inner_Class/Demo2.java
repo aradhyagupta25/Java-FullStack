dcd5b4b5d55deb339fe31297aa8a5e3747692ed6
Aradhya Gupta <personalsomu25@gmail.com>
2025-11-11 15:50:08 +0530

    Anonymous Inner Class.

--- Java_Advance/Inner_Class/Demo2.java
+++ Java_Advance/Inner_Class/Demo2.java
@@ -14,12 +14,25 @@ class A{
 
 public class Demo2 {
     public static void main(String[] args) {
+
         A obj = new A()
-        {   // implementing an inner class with no name of it. ANONYMOUS INNER CLASS.
-            public void show() {
-                System.out.println("Inside New Show");
-            }
-        };
+            {
+                public void show() {
+                    System.out.println("Inside New Show");
+                }
+            };      // implementing an inner class with no name of it. ANONYMOUS INNER CLASS.
         obj.show();
+
+        Mammal lion = new Mammal() {
+            @Override
+            public void Name() {
+                System.out.println("Lion");
+            }
+        };      // here we are not making object of abstract class "Mammal", but we are creating object of an Anonymous class which is overriding the abstract method.
+
     }
+}
+
+abstract class Mammal{
+    public abstract void Name();
 }
