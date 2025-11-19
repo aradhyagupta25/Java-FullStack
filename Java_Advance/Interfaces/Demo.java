8b2544344eddbccbc116c830bcf7b1410f60a5b5
Aradhya Gupta <personalsomu25@gmail.com>
2025-11-19 12:18:16 +0530

    Interfaces

--- Java_Advance/Interfaces/Demo.java
+++ Java_Advance/Interfaces/Demo.java
@@ -4,8 +4,21 @@ interface A{ // interface is not a clas like abstract class. and by default ever
 
     void show();
     void config();
+}
 
+class B implements A{   // defining methods of interface A in B otherwise B will become abstract class.
+    public void show(){
+        System.out.println("Inside Show");
+    }
+    public void config(){
+        System.out.println("Inside Config");
+    }
 }
 
 public class Demo {
+    public static void main(String[] args) {
+        A obj = new B();
+        obj.show();
+        obj.config();
+    }
 }
