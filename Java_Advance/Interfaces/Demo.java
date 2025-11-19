7c8d68e610709e4915f45d01f2e98ce121d9055a
Aradhya Gupta <personalsomu25@gmail.com>
2025-11-19 16:47:49 +0530

    Interfaces

--- Java_Advance/Interfaces/Demo.java
+++ Java_Advance/Interfaces/Demo.java
@@ -8,21 +8,38 @@ interface A{ // interface is not a clas like abstract class. and by default ever
     void config();
 }
 
-class B implements A{   // defining methods of interface A in B otherwise B will become abstract class. "implements" are only for mehtods.
+interface X{
+    void run();
+}
+
+interface Y extends X{
+    void stop(int y);   // can pass arguments.
+}
+
+class B implements A, Y{   // defining methods of interface A in B otherwise B will become abstract class. "implements" are only for methods.
     public void show(){
         System.out.println("Inside Show");
     }
     public void config(){
         System.out.println("Inside Config");
     }
+    public void run(){  // can de defined as Y inherited X.
+        System.out.println("Inside Run");
+    }
+    public void stop(int y){
+        System.out.println("Inside Stop: " + y );
+    }
 }
 
 public class Demo {
     public static void main(String[] args) {
-        A obj = new B();
+        A obj = new B();    // type A : object of B
         obj.show();
         obj.config();
 
+        Y obj2 = new B();      // type Y : object of B
+        obj2.run();
+        obj2.stop(7);
         System.out.println(A.year);
         System.out.println(A.city);
 
