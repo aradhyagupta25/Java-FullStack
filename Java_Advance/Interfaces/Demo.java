d7a6881d5531401009bdd84d62ac3fbb6b71cb81
Aradhya Gupta <personalsomu25@gmail.com>
2025-11-19 16:39:23 +0530

    Interfaces

--- Java_Advance/Interfaces/Demo.java
+++ Java_Advance/Interfaces/Demo.java
@@ -2,11 +2,13 @@ package Java_Advance.Interfaces;
 
 interface A{ // interface is not a clas like abstract class. and by default every method of interface is "public abstract".
 
+    int year = 2027;    // interface variables are by default final and static.
+    String city = "SF";
     void show();
     void config();
 }
 
-class B implements A{   // defining methods of interface A in B otherwise B will become abstract class.
+class B implements A{   // defining methods of interface A in B otherwise B will become abstract class. "implements" are only for mehtods.
     public void show(){
         System.out.println("Inside Show");
     }
@@ -20,5 +22,10 @@ public class Demo {
         A obj = new B();
         obj.show();
         obj.config();
+
+        System.out.println(A.year);
+        System.out.println(A.city);
+
+        //A.year = 2026;
     }
 }
