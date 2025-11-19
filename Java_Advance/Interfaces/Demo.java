593cca4f839fe8910b0f298e46bc78484658693a
Aradhya Gupta <personalsomu25@gmail.com>
2025-11-19 16:55:41 +0530

    Interfaces

--- Java_Advance/Interfaces/Demo.java
+++ Java_Advance/Interfaces/Demo.java
@@ -1,5 +1,9 @@
 package Java_Advance.Interfaces;
 
+// class - class -> extends.
+// class - interface -> implement.
+// interface - interface -> extends.
+
 interface A{ // interface is not a clas like abstract class. and by default every method of interface is "public abstract".
 
     int year = 2027;    // interface variables are by default final and static.
