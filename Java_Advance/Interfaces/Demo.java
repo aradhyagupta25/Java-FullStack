802b499dd43510e9bfe7fa269a7ea9adfa0050ab
Aradhya Gupta <personalsomu25@gmail.com>
2025-11-19 10:50:12 +0530

    Interfaces

---
+++ Java_Advance/Interfaces/Demo.java
@@ -0,0 +1,10 @@
+package Java_Advance.Interfaces;
+
+interface A{ // interface is not a clas like abstract class. and by default every method of interface is "public abstract".
+
+
+
+}
+
+public class Demo {
+}
