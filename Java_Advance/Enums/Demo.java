45f440e02620f5c77cdb4ab893ee0f2b56dcbf9e
Aradhya Gupta <personalsomu25@gmail.com>
2025-11-20 22:21:52 +0530

    enums

--- Java_Advance/Enums/Demo.java
+++ Java_Advance/Enums/Demo.java
@@ -3,7 +3,7 @@ package Java_Advance.Enums;
 //an enum (short for enumeration) is a special kind of class that represents a fixed set of named constants. While it uses the enum keyword instead of class, it shares many characteristics with regular classes and can even contain methods, fields, and constructors.
 
 enum status{    // status is a class here.
-    running , failed, pending, success; // these are objects of the class status.
+    running , failed, pending, success; // "NAMED CONSTANTS".  these are objects of the class status.
 }
 
 public class Demo {
@@ -11,5 +11,9 @@ public class Demo {
 
         status s = status.running;  // type: status.
         System.out.println(s);
+        System.out.println(s.ordinal());    // prints no. of that named constant.
+
+        status[] t = status.values();   // returns array of all values.
+        for(status stat : t) System.out.println(stat + " : " + stat.ordinal());
     }
 }
