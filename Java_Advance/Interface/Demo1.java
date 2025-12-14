e901bfea21b2b8f7b41fcc0f79939e904fd85761
Aradhya Gupta <personalsomu25@gmail.com>
2025-12-14 23:04:14 +0530

    Interfaces

---
+++ Java_Advance/Interface/Demo1.java
@@ -0,0 +1,7 @@
+package Java_Advance.Interface;
+// All the Interface are Public Abstract.
+// Types: Normal, Functional/Single abstract method, Marker.
+
+// Serialization: saving data of any object in hard drive. this can be done using marker interface.
+public class Demo1 {
+}
