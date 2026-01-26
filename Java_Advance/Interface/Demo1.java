cfa958363d7c74a5e7b700f7ecee9c8f8cb51f91
Aradhya Gupta <personalsomu25@gmail.com>
2026-01-26 18:33:00 +0530

    Daily Commit

--- Java_Advance/Interface/Demo1.java
+++ Java_Advance/Interface/Demo1.java
@@ -5,5 +5,25 @@ package Java_Advance.Interface;
 // Serialization: saving data of any object in hard drive. this can be done using marker interface.
 
 //A functional interface in Java is any interface that contains exactly one abstract method.
-public class Demo1 {
+
+@FunctionalInterface
+interface A{
+    void show();    //only one method for functional Interface.
+}
+
+class B implements A{
+    public void show(){
+        System.out.println("Inside Show");
+    }
+}
+public class Demo1{
+    public static void main(String[] args) {
+        A obj =new A() {
+            @Override
+            public void show() {
+                System.out.println("Inside show FI");
+            }
+        };
+        obj.show();
+    }
 }
