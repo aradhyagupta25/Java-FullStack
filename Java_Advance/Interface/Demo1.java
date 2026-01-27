ea9953aebe7d3ad04552e8ae3519e8cf1cb2b21c
Aradhya Gupta <personalsomu25@gmail.com>
2026-01-27 17:11:43 +0530

    lambda exp

--- Java_Advance/Interface/Demo1.java
+++ Java_Advance/Interface/Demo1.java
@@ -18,12 +18,19 @@ class B implements A{       // one way of overriding method of interface.
 }
 public class Demo1{
     public static void main(String[] args) {
-        A obj =new A() {        // incase of functional interface we can directly define the abstract method of the interface here.
+        // in case of functional interface we can directly define the abstract method of the interface here.
+        A obj =new A() {
             @Override
             public void show() {
                 System.out.println("Inside show FI");
             }
         };
+
+        //now using lambda expression. "->" is called the lambda. code inside {} is called an expression.
+        A ob2j = ( ) -> {
+                System.out.println("Inside show lambda");
+            };
         obj.show();
+        ob2j.show();
     }
 }
